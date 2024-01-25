6286a188991cff1688c3b2193e746af7818631f8
ethan <ethanbargaudarm@gmail.com>
2024-01-26 00:15:13 +0100

    Added obsidian_path option + Changed get_obsidian_folder to get_config_folder for a more general   options

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,59 @@
 use clap::Parser;
 use core::panic;
-use serde_json::{from_str, Value};
 use std::env;
-use std::fs;
-use std::path;
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// Path to the obsidian config folder, if not specified the folder will be searched in the
+    /// system default location.
+    #[arg(long)]
+    #[arg(value_parser = opt_folder_path)]
+    obsidian_path: Option<PathBuf>,
+
+    /// Path to the obsi-tool config folder, if not specified the file will be searched in the system
     /// default location.
     #[arg(short, long)]
-    config_path: Option<String>,
+    #[arg(value_parser = opt_folder_path)]
+    config_path: Option<PathBuf>,
 }
 
-fn get_obsidian_folder() -> PathBuf {
-    let config_string = match env::consts::OS {
-        "linux" => {
-            let mut config_string =
-                env::var("HOME").expect("Couldn't find HOME folder") + "/.config/obsidian";
-            if !path::Path::new(&config_string).exists() {
-                config_string = match env::var("XDG_CONGIG_HOME") {
-                    Ok(s) => s + "/obsidian",
-                    Err(_) => panic!("Could not find obsidian config folder"),
-                };
-            }
-            config_string
+fn opt_folder_path(s: &str) -> Result<PathBuf, String> {
+    let obsi_path = PathBuf::from(s);
+    if obsi_path.exists() {
+        if obsi_path.is_dir() {
+            Ok(obsi_path)
+        } else {
+            Err(format!("The specified file isn't a folder"))
         }
-        "windows" => env::var("APPDATA").expect("Couldn't find APPDATA folder") + "\\obsidian",
+    } else {
+        Err(format!("The specified folder doesn't exist"))
+    }
+}
+
+fn get_config_folder(name: Option<&str>) -> PathBuf {
+    let config_base_path = PathBuf::from(match env::consts::OS {
+        "linux" => match env::var("XDG_CONFIG_HOME") {
+            Ok(s) => s,
+            Err(_) => env::var("HOME").expect("Couldn't find HOME folder") + "/.config",
+        },
+        "windows" => env::var("APPDATA").expect("Couldn't find APPDATA folder"),
         "macos" => {
             "/Users/".to_string()
                 + &env::var("USER").expect("Couldn't find USER folder")
-                + "/Library/Application Support/obsidian"
+                + "/Library/Application Support"
         }
         _ => panic!("System is not supported!"),
+    });
+
+    let config_path = match name {
+        Some(name) => config_base_path.join(name),
+        None => config_base_path,
     };
 
-    let config_path = PathBuf::from(config_string);
-
     if !config_path.exists() {
-        panic!("Could not find obsidian config folder");
+        panic!("Could not find config folder at {}", config_path.display());
     }
 
     return config_path;
@@ -49,12 +62,17 @@ fn get_obsidian_folder() -> PathBuf {
 fn main() {
     let cli = Cli::parse();
 
-    let obsi_path = match cli.config_path {
-        Some(s) => PathBuf::from(s),
-        None => get_obsidian_folder(),
-    };
+    let obsi_path = cli
+        .obsidian_path
+        .unwrap_or_else(|| get_config_folder(Some("obsidian")));
 
-    println!("Path: {:?}", obsi_path);
+    println!("Obsidian config folder: {}", obsi_path.display());
+
+    let config_path = cli
+        .config_path
+        .unwrap_or_else(|| get_config_folder(Some("obsi-tool")));
+
+    println!("Config folder path: {}", config_path.display());
 }
 
 #[cfg(test)]

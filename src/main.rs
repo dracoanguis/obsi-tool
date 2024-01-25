2750f000f7e692cbf9551847fb9ced783ab1712d
ethan <ethanbargaudarm@gmail.com>
2024-01-25 01:07:46 +0100

    Finished auto find of obsidian folder for linux

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,16 @@ fn get_obsidian_folder() -> String {
             let config_string =
                 env::var("HOME").expect("Should be able to find $HOME") + "/.config/obsidian";
             if !path::Path::new(&config_string).exists() {
-                let config_string = 
+                let config_string = match env::var("XDG_CONGIG_HOME") {
+                    Ok(s) => s + "/obsidian",
+                    Err(_) => panic!("Could not find obsidian config folder"),
+                };
+
+                if !path::Path::new(&config_string).exists() {
+                    panic!("Could not find obsidian config folder");
+                }
+
+                return config_string;
             }
 
             return config_string;
@@ -20,8 +29,14 @@ fn get_obsidian_folder() -> String {
 }
 
 fn main() {
-    let file_path = "/home/gargan/.config/obsidian/obsidian.json";
-    println!("In file {}", file_path);
+    // System dependant string
+    let obsidian_string = get_obsidian_folder();
+
+    // System independant path
+    let obsidian_path = path::Path::new(&obsidian_string);
+
+    let file_path = obsidian_path.join("obsidian.json");
+    println!("In file {}", file_path.display());
 
     let contents = fs::read_to_string(file_path).expect("Should be able to read");
 

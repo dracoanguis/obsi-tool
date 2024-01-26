87316d5ce31fb052c3ed4e02ad93459223e3c439
ethan <ethanbargaudarm@gmail.com>
2024-01-26 01:35:46 +0100

    Added basic vault extraction

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
-use core::panic;
-use std::env;
+use core::{fmt, panic};
+use serde_json::{from_str, Value};
 use std::path::PathBuf;
+use std::{env, fs};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -17,6 +18,18 @@ struct Cli {
     #[arg(short, long)]
     #[arg(value_parser = opt_folder_path)]
     config_path: Option<PathBuf>,
+}
+
+#[derive(Debug)]
+struct Vault {
+    pub id: String,
+    pub path: PathBuf,
+}
+
+impl fmt::Display for Vault {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "id:{} path:{}", self.id, self.path.display())
+    }
 }
 
 fn opt_folder_path(s: &str) -> Result<PathBuf, String> {
@@ -59,6 +72,36 @@ fn get_config_folder(name: Option<&str>) -> PathBuf {
     return config_path;
 }
 
+fn get_vault_list(obsidian_path: &PathBuf) -> Vec<Vault> {
+    let obsidian_manifest_path = obsidian_path.join("obsidian.json");
+
+    if !obsidian_manifest_path.exists() {
+        panic!(
+            "Cannot find obsidian file at {}",
+            obsidian_manifest_path.display()
+        );
+    }
+
+    if !obsidian_manifest_path.is_file() {
+        panic!("{} is not a file", obsidian_manifest_path.display());
+    }
+
+    let file_value: Value = from_str(
+        &fs::read_to_string(obsidian_manifest_path).expect("Obsidian file should be readable"),
+    )
+    .expect("Obsidian file should be parseable");
+
+    return file_value["vaults"]
+        .as_object()
+        .unwrap()
+        .into_iter()
+        .map(|(id, val)| Vault {
+            id: id.to_owned(),
+            path: PathBuf::from(val["path"].as_str().unwrap()),
+        })
+        .collect();
+}
+
 fn main() {
     let cli = Cli::parse();
 
@@ -68,12 +111,18 @@ fn main() {
 
     println!("Obsidian config folder: {}", obsi_path.display());
 
-    let config_path = cli
-        .config_path
-        .unwrap_or_else(|| get_config_folder(Some("obsi-tool")));
+    let config_path = cli.config_path.unwrap_or_else(|| get_config_folder(None)); //Some("obsi-tool")));
 
     println!("Config folder path: {}", config_path.display());
-}
 
-#[cfg(test)]
-mod tests {}
+    let vaults = get_vault_list(&obsi_path);
+
+    vaults.iter().for_each(|v| {
+        println!(
+            "{}\n Exists: {}; Is dir: {}",
+            v,
+            v.path.exists(),
+            v.path.is_dir()
+        )
+    });
+}
